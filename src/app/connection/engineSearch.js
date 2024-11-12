ddf7caca57e341513a0a6a92e63780be3ca2b074
marcos vinicius <marcos.vinicius7170@gmail.com>
2024-11-12 13:42:34 -0300

    name source change : release

--- src/app/connection/engineSearch.js
+++ src/app/connection/engineSearch.js
@@ -26,7 +26,7 @@ async function searchSync() {
 
                     const body = {
                         name: index.deal_custom_fields.filter(res => res.custom_field.label.includes('Nome  do responsável')).map(res => res.value)[0] ? index.deal_custom_fields.filter(res => res.custom_field.label.includes('Nome  do responsável')).map(res => res.value)[0] : "Sem este dado no rd",
-                        owner: index.user.name ? index.user.name : "Sem este dado no rd",
+                        owner: index.deal_custom_fields.filter(res => res.custom_field.label.includes('Vendedor')).map(res => res.value)[0] ? index.deal_custom_fields.filter(res => res.custom_field.label.includes('Vendedor')).map(res => res.value)[0] : "Sem este dado no rd",
 
                         unidade: index.deal_custom_fields.filter(res => res.custom_field.label.includes('Unidade')).map(res => res.value)[0] ? index.deal_custom_fields.filter(res => res.custom_field.label.includes('Unidade')).map(res => res.value)[0] : "Sem este dado no rd",
                         background: index.deal_custom_fields.filter(res => res.custom_field.label.includes('Background')).map(res => res.value)[0] ? index.deal_custom_fields.filter(res => res.custom_field.label.includes('Background')).map(res => res.value)[0] : "Sem este dado no rd",
