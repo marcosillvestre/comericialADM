ae40841383b2453d76dc7ac4fcadbcfc3277b867
Marcos <88978591+marcosillvestre@users.noreply.github.com>
2025-01-28 16:25:20 -0300

    Update postConttroller.js

--- src/app/controllers/internal/postConttroller.js
+++ src/app/controllers/internal/postConttroller.js
@@ -35,7 +35,7 @@ class PostController {
                     for (const index of response?.data?.deals) {
 
                         const body = {
-                            name: index.deal_custom_fields.filter(res => res.custom_field.label.includes('Nome do responsável')).map(res => res.value)[0] ? index.deal_custom_fields.filter(res => res.custom_field.label.includes('Nome  do responsável')).map(res => res.value)[0] : "Sem este dado no rd",
+                            name: index.deal_custom_fields.filter(res => res.custom_field.label.includes('Nome do responsável')).map(res => res.value)[0] ? index.deal_custom_fields.filter(res => res.custom_field.label.includes('Nome do responsável')).map(res => res.value)[0] : "Sem este dado no rd",
                             owner: index.user.name ? index.user.name : "Sem este dado no rd",
                             unidade: index.deal_custom_fields.filter(res => res.custom_field.label.includes('Unidade')).map(res => res.value)[0] ? index.deal_custom_fields.filter(res => res.custom_field.label.includes('Unidade')).map(res => res.value)[0] : "Sem este dado no rd",
                             background: index.deal_custom_fields.filter(res => res.custom_field.label.includes('Background')).map(res => res.value)[0] ? index.deal_custom_fields.filter(res => res.custom_field.label.includes('Background')).map(res => res.value)[0] : "Sem este dado no rd",
