1bf6749c1604b31b8fedd8f6dff47f9c8e13defb
Marcos <88978591+marcosillvestre@users.noreply.github.com>
2025-01-28 16:31:41 -0300

    Update postConttroller.js

--- src/app/controllers/internal/postConttroller.js
+++ src/app/controllers/internal/postConttroller.js
@@ -233,7 +233,7 @@ class PostController {
                         const deal = index.deal_custom_fields
                         const desPrimeirasParcelas = deal.filter(res => res.custom_field.label.includes('Valor do desconto primeiras parcelas')).map(res => res.value)[0]
                         const body = {
-                            name: deal.filter(res => res.custom_field.label.includes('Nome  do responsável')).map(res => res.value)[0],
+                            name: deal.filter(res => res.custom_field.label.includes('Nome do responsável')).map(res => res.value)[0],
                             contrato: deal.filter(res => res.custom_field.label.includes('Nº do contrato')).map(res => res.value)[0],
                             unidade: deal.filter(res => res.custom_field.label.includes('Unidade')).map(res => res.value)[0],
                             rg: deal.filter(res => res.custom_field.label.includes('RG responsável')).map(res => res.value)[0],
