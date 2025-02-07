83f3a71141fbf2ce7ff9f7f55bc4410039175dfa
marcos vinicius <marcos.vinicius7170@gmail.com>
2025-02-07 13:09:17 -0300

    starting the cron job : hotfix

--- src/app/core/cronManager.js
+++ src/app/core/cronManager.js
@@ -37,7 +37,7 @@ const functionsArray = [
 functionsArray.forEach(res => {
     return new CronJob(res.time,
         function () {
-            // res.fn()
+            res.fn()
         },
         null,
         true,
