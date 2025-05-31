5e8f861e485dee84c596fd6e173e3ace58bbcaef
marcos vinicius <marcos.vinicius7170@gmail.com>
2025-05-31 13:14:00 -0300

    feat: clear pedagogical observations in contract templates for better readability

--- src/app/controllers/external/contaAzulRegister.js
+++ src/app/controllers/external/contaAzulRegister.js
@@ -227,7 +227,7 @@ Turma: ${dataPrimeiraAula} de ${horarioInicio} às ${horarioFim}
 Professor: ${Professor}
 Carga horária: ${cargaHoraria} 
 Unidade: ${Unidade}
-Observações pedagógicas: ${observacaoFinanceiro}
+Observações pedagógicas: 
 Observações financeiras:${observacaoPedagogico}
 id: ${id}
 serviço: parcela
@@ -482,7 +482,7 @@ Turma: ${dataPrimeiraAula} de ${horarioInicio} às ${horarioFim}
 Professor: ${Professor}
 Carga horária: ${cargaHoraria} 
 Unidade: ${Unidade}
-Observações pedagógicas: ${observacaoFinanceiro}
+Observações pedagógicas: 
 Observações financeiras:${observacaoPedagogico}
 id: ${id}
 serviço: material didatico
@@ -846,7 +846,7 @@ Turma: ${dataPrimeiraAula} de ${horarioInicio} às ${horarioFim}
 Professor: ${Professor}
 Carga horária: ${cargaHoraria} 
 Unidade: ${Unidade}
-Observações pedagógicas: ${observacaoFinanceiro}
+Observações pedagógicas: 
 Observações financeiras:${observacaoPedagogico}
 id: ${id}
 serviço: taxa de matricula
