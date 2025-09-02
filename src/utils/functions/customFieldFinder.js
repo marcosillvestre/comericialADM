0785ee00a98565dc17f8e75571dbb9933b212e63
marcos vinicius <marcos.vinicius7170@gmail.com>
2025-09-02 17:43:05 -0300

    fix: enhance bodyMakerForCustomFields to include services and improve serviceName handling

--- src/utils/functions/customFieldFinder.js
+++ src/utils/functions/customFieldFinder.js
@@ -83,6 +83,9 @@ export const bodyMakerForCustomFields = async (contractData) => {
     const vendedor = findYourValueForCustomFields("Vendedor", deal.deal_custom_fields) ?
         findYourValueForCustomFields("Vendedor", deal.deal_custom_fields) : deal.user.name
 
+
+    console.log(deal.deal_products)
+
     return {
         ...data?.customFields,
         id: deal.id,
@@ -93,6 +96,7 @@ export const bodyMakerForCustomFields = async (contractData) => {
         CelularResponsavel: phone,
         valorCurso: deal.deal_products[0]?.total,
         service: deal.deal_products[0]?.name,
+        services: deal.deal_products
     }
 
 }
@@ -102,8 +106,8 @@ export const bodyFilterCustomFields = async (deal) => {
 
     const { id, deal_custom_fields, deal_products: [service], contacts, created_at } = deal
 
-    const serviceName = service?.name || undefined
-    const Subclasse = serviceName ? serviceName.split(' - ')[1] : serviceName;
+    const serviceName = service?.name;
+    const Subclasse = serviceName.includes('-') ? serviceName.split(' - ')[1] : serviceName;
 
     return {
         created_at,
