0f417b64135129c6ed9c056ecc6d51fc976a9036
marcos vinicius <marcos.vinicius7170@gmail.com>
2025-09-02 17:56:13 -0300

    fix: refine serviceName extraction and Subclasse assignment in bodyFilterCustomFields function

--- src/utils/functions/customFieldFinder.js
+++ src/utils/functions/customFieldFinder.js
@@ -106,8 +106,9 @@ export const bodyFilterCustomFields = async (deal) => {
 
     const { id, deal_custom_fields, deal_products: [service], contacts, created_at } = deal
 
-    const serviceName = service?.name || undefined
-    const Subclasse = serviceName ? serviceName.split(' - ')[1] : serviceName;
+    const serviceName = service?.name;
+    const Subclasse = serviceName.includes('-') ? serviceName.split(' - ')[1] : serviceName;
+
 
     return {
         created_at,
